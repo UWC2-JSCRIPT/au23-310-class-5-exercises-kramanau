d1c561582af3d6ec2165c7381b563735da6f9fa8
Karolis Ramanauskas <kramanauskas112@gmail.com>
2023-11-14 00:22:35 -0500

    accessing elements completed

--- accessing.js
+++ accessing.js
@@ -1,5 +1,9 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
-
+document.getElementById('weather-head').innerText = "February 10 Weather Forecast, Seattle"
 // Change the styling of every element with class "sun" to set the color to "orange"
-
+const suns = document.getElementsByClassName('sun')
+for (i = 0; i < suns.length; i++){
+    suns[i].style.color = 'orange'
+};
 // Change the class of the second <li> from to "sun" to "cloudy"
+suns[1].className = 'cloudy';
