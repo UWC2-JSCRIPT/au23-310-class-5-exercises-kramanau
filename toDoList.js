24aeba4b4920a470a20ac022a88e729f0519dc2e
Karolis Ramanauskas <kramanauskas112@gmail.com>
2023-11-14 18:22:27 -0500

    completed toDoList problems

--- toDoList.js
+++ toDoList.js
@@ -1,6 +1,20 @@
 // If an li element is clicked, toggle the class "done" on the <li>
-
+const liList = document.querySelectorAll('li');
+liList.forEach((liEl) => {
+  liEl.addEventListener('click', () => {
+    liEl.classList.toggle('done');
+  });
+});
 // If a delete link is clicked, delete the li element / remove from the DOM
+const list = document.querySelectorAll('li');
+list.forEach((liEl) => {
+  const button = liEl.getElementsByTagName('a')[0];
+  button.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const parent = document.getElementsByClassName('today-list')[0];
+    parent.removeChild(liEl);
+  });
+})
 
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
@@ -10,5 +24,31 @@ const addListItem = function(e) {
   const input = this.parentNode.getElementsByTagName('input')[0];
   const text = input.value; // use this text to create a new <li>
 
+  const parent = document.getElementsByClassName('today-list')[0];
+  const newItem = document.createElement('li');
+  newItem.addEventListener('click', () => {
+    newItem.classList.toggle('done');
+  });
+
+  const textNode = document.createElement('span');
+  textNode.textContent = text;
+  newItem.appendChild(textNode);
+
+  // Added so spacing looks correct.
+  newItem.appendChild(document.createTextNode(' '));
+
+  const deleteButton = document.createElement('a');
+  deleteButton.textContent = 'Delete';
+  deleteButton.className = 'delete';
+  deleteButton.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const parent = document.getElementsByClassName('today-list')[0];
+    parent.removeChild(newItem);
+  });
+  newItem.appendChild(deleteButton);
+
+  parent.appendChild(newItem);
   // Finish function here
 };
+
+document.getElementsByClassName('add-item')[0].addEventListener('click', addListItem);
